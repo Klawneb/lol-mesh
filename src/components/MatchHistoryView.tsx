7ff7e30c8f8c3d5d0bab61c7bbc16cb1370daf2d
Benjamin Walker <benjaminwalker222@gmail.com>
2023-03-15 03:18:35 +0000

    limit amount of matches shown

--- src/components/MatchHistoryView.tsx
+++ src/components/MatchHistoryView.tsx
@@ -3,6 +3,7 @@ import { formatDistanceStrict } from "date-fns";
 import Image from "next/image";
 import { MatchWithParticipants } from "../utils/types";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useState } from "react";
 
 interface MatchHistoryViewProps {
   matchHistory: MatchWithParticipants[];
@@ -11,10 +12,12 @@ interface MatchHistoryViewProps {
 
 export default function MatchHistoryView({ matchHistory, puuid }: MatchHistoryViewProps) {
   const [animationParent] = useAutoAnimate();
+  const [matchesShown, setMatchesShown] = useState(10);
 
   return (
     <div ref={animationParent} className="flex flex-col flex-grow h-0 overflow-scroll w-full scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-primary mt-2">
       {matchHistory
+        .slice(matchHistory.length - matchesShown, matchHistory.length)
         .sort((a, b) => b.startTime.valueOf() - a.startTime.valueOf())
         .map((match) => {
           const participation = match.participants.find((participant) => {
@@ -23,7 +26,7 @@ export default function MatchHistoryView({ matchHistory, puuid }: MatchHistoryVi
           if (participation) {
             const champion = participation.champion === "FiddleSticks" ? "Fiddlesticks" : participation.champion;
             return (
-              <div key={match.id} className={`flex w-full mb-2 min-h-[75px] rounded-lg p-2 justify-between shadow-md items-center ${participation.win ? "bg-base-100" : "bg-base-100"}`}>
+              <div key={match.id} className={`flex w-full mb-2 min-h-[75px] rounded-lg p-2 justify-between shadow-md items-center bg-base-100`}>
                 <Image alt="summoner-icon" src={`https://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/${champion}.png`} width={128} height={128} className="w-16 h-16 rounded-lg" />
                 <div className="w-1/3">
                   <p className="text-base-content font-bold text-3xl text-center">
@@ -39,6 +42,15 @@ export default function MatchHistoryView({ matchHistory, puuid }: MatchHistoryVi
           }
           return null;
         })}
+
+      {matchHistory.length > 0 ? (
+        <div
+          className="flex w-full mb-2 min-h-[75px] rounded-lg p-2 shadow-md items-center bg-base-100 justify-center text-3xl hover:cursor-pointer transition-all hover:b"
+          onClick={() => setMatchesShown((prev) => prev + 10)}
+        >
+          Load more
+        </div>
+      ) : null}
     </div>
   );
 }
